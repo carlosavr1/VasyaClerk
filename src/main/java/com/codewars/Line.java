747f58c5ddd452d28ac8a09be1feb1be39faf61a
SURAMERICANA\carlvarn <1037636590>
2017-07-18 08:16:59 -0500

    Two test proposed by Codewars Empty main class Maven compiler JUnit dependecy .gitignore for Intellij IDEA

---
+++ src/main/java/com/codewars/Line.java
@@ -0,0 +1,11 @@
+package com.codewars;
+
+/**
+ * Created by carlvarn on 15/07/2017.
+ */
+public class Line {
+    public static String Tickets(int[] peopleInLine)
+    {
+       return "";
+    }
+}
